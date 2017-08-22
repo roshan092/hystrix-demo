4e88335ee0e87415ed207b4f85dece7331c3e2c5
Roshan <roshan092@gmail.com>
2017-08-22 23:24:03 +1000

    Move hardcoded endpoints to property files

--- discovery-app/src/main/java/com/roshan092/DiscoveryService.java
+++ discovery-app/src/main/java/com/roshan092/DiscoveryService.java
@@ -2,28 +2,36 @@ package com.roshan092;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
-@Component
+@Service
 public class DiscoveryService {
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${movies.base.url}")
+    private String moviesBaseUrl;
+
+    @Value("${songs.base.url}")
+    private String songsBaseUrl;
+
     @HystrixCommand(fallbackMethod = "getCurrentTopGrossingMovie")
     public String getTopGrossingMovie(@PathVariable Integer year) {
-        URI uri = UriComponentsBuilder.fromUriString("http://localhost:9090/movies/" + year).build().toUri();
+        URI uri = UriComponentsBuilder.fromUriString(moviesBaseUrl + "/movies/" + year).build().toUri();
         return restTemplate.getForObject(uri, String.class);
     }
 
     @HystrixCommand(fallbackMethod = "getCurrentTopGrossingSong")
     public String getTopGrossingSong(@PathVariable Integer year) {
-        URI uri = UriComponentsBuilder.fromUriString("http://localhost:9091/songs/" + year).build().toUri();
+        URI uri = UriComponentsBuilder.fromUriString(songsBaseUrl + "/songs/" + year).build().toUri();
         return restTemplate.getForObject(uri, String.class);
     }
 
