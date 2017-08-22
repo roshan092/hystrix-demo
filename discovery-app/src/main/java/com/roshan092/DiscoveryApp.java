b66f3fd97328188eb415f6539b0b28187f41f618
Roshan <roshan092@gmail.com>
2017-08-23 02:05:04 +1000

    Working hyetrix dashboard

--- discovery-app/src/main/java/com/roshan092/DiscoveryApp.java
+++ discovery-app/src/main/java/com/roshan092/DiscoveryApp.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import java.net.URI;
 @SpringBootApplication
 @RestController
 @EnableCircuitBreaker
+@EnableDiscoveryClient
 public class DiscoveryApp {
 
     @Autowired
