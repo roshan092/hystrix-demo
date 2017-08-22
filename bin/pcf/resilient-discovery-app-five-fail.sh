c59769f7e66b19955856e1fd3d0a4745a300cbaf
Roshan <roshan092@gmail.com>
2017-08-22 23:22:14 +1000

    Add new scripts for pcf

---
+++ bin/pcf/resilient-discovery-app-five-fail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in {1..500}
+do
+    YEAR=$(( $RANDOM % 10 + 2006 ))
+    URL=https://discovery-app-rjl.cfapps.io/topgrossing/${YEAR}
+    echo ${URL}
+	curl ${URL} ; echo
+	SLEEP_INTERVAL=$(( $RANDOM % 10 % 5 ))
+	sleep .${SLEEP_INTERVAL}
+done
